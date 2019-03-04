c99b6041d38f5fcc260b3b3cb201b89d7ba74620
Sean Cahill <seancahill25@gmail.com>
2019-03-04 12:25:09 -0500

    app that calculates how many times a ball will pass by a window

--- BouncingBall/Program.cs
+++ BouncingBall/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace BouncingBall
 {
-    class Program
+    class BouncingBall
     {
         static void Main(string[] args)
         {
+            Console.Write("Input Drop Height:");
+            double h = float.Parse(Console.ReadLine());
+            Console.Write("Input Bounce Rate:");
+            double bounce = float.Parse(Console.ReadLine());
+            Console.Write("Input Window Height:");
+            double window = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Result:" + BouncingBalls(h,bounce,window));
+        }
+
+        public static int BouncingBalls(double h, double bounce, double window)
+        {
+            var sightings = 1;
+            if (bounce >= 1 || bounce <= 0 || h < window || h <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                if(h*bounce > window)
+                {
+                    sightings += 2;
+                }
+            }
+            return sightings;
         }
     }
 }
